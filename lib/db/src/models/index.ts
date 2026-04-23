dc3a08d68a6325826d44bd51751c7b6828f7529a
3enawi <147712033+3enawi@users.noreply.github.com>
2026-04-23 21:59:48 +0400

    123

--- lib/db/src/models/index.ts
+++
@@ -1,5 +0,0 @@
-export { StreamerModel, type Streamer } from "./streamer";
-export { ChannelRequestModel, type ChannelRequest } from "./channelRequest";
-export { FavoriteModel, type Favorite } from "./favorite";
-export { AdminEmailModel, type AdminEmail } from "./adminEmail";
-export { CounterModel, nextSequence } from "./counter";
